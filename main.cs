226b9d04c82f50f1fe7340c564c5e62b17a6df78
AlbertSM-dev <71648626+AlbertSM-dev@users.noreply.github.com>
2024-02-07 15:43:31 +0000

    Create main.cs

---
+++ main.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int StrStr(string haystack, string needle) {
+        if(!haystack.Contains(needle))
+        {
+            return -1;
+        }
+        else
+        {
+            return haystack.IndexOf(needle);
+        }
+    }
+}
